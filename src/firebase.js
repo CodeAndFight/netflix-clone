4179934be292d8721ddd4e34697e49ba2a7c9c9b
CodeAndFight <dawidkuczmarski3@gmail.com>
2022-11-03 21:32:18 +0100

    setting netflix-clone project&firebase

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBxh706P2XkQT1ovNlCiDW0rxd9oUCPEHg",
+  authDomain: "netlix-clone-c48ad.firebaseapp.com",
+  projectId: "netlix-clone-c48ad",
+  storageBucket: "netlix-clone-c48ad.appspot.com",
+  messagingSenderId: "968093756590",
+  appId: "1:968093756590:web:85c89f999dada8ed72513f",
+  measurementId: "G-92ZTDH6D8S",
+};
