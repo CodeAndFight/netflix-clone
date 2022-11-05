d5a55c1a5d23d5e8c2daf0cea235b89198eb2597
CodeAndFight <dawidkuczmarski3@gmail.com>
2022-11-05 18:20:12 +0100

    buildining nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Nav.css";
+import logo from "../assets/logo-vector.png";
+import avatar from "../assets/netflix-avatar.png";
 
 function Nav() {
+  const [show, handleShow] = useState(false);
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+
   return (
-    <div className="nav">
-      <h1>Here is nav</h1>
+    <div className={`nav ${show && "nav_black"}`}>
+      <div className="nav-contents">
+        <img className="netflix_logo" src={logo} alt="Netflix big logo" />
+
+        <img
+          className="netflix_avatar"
+          src={avatar}
+          alt="Netflix Avatar Red "
+        />
+      </div>
     </div>
   );
 }
